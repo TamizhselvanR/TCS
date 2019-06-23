9d9a77cb7328e5ded70904d5e27760d9f17f7071
Tamizhselvanr <rvtamizhselvan@gmail.com>
2019-06-23 19:09:43 +0530

    Revert "Added a Code to find LCM of 2 numbers"

---
+++ lcm.py
@@ -0,0 +1,11 @@
+def lcm(a,b,maxi):
+    if(maxi % a == 0 and maxi % b == 0):
+        return maxi;
+    maxi += 1
+    return lcm(a,b,maxi)
+
+if __name__ == '__main__':
+    a = 22
+    b = 6
+    maxi = max(a,b)
+    print(lcm(a,b,maxi))
